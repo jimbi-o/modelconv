6485458ab29251c8fd5ee378aeb3770e5d067407
jimbi-o <35837796+jimbi-o@users.noreply.github.com>
2022-11-16 08:54:56 +0900

    gfxmath library added

--- include/modelconv/modelconv.h
+++ include/modelconv/modelconv.h
@@ -1,5 +1,6 @@
 #ifndef MINIMAL_CPP_PJ_H
 #define MINIMAL_CPP_PJ_H
+#include <cstdint>
 namespace modelconv {
 }
 #endif
--- src/modelconv.cpp
+++ src/modelconv.cpp
@@ -2,9 +2,31 @@
 #include "assimp/Importer.hpp"
 #include "assimp/scene.h"
 #include "assimp/postprocess.h"
+#include "gfxminimath/gfxminimath.h"
 #include "doctest/doctest.h"
 TEST_CASE("load model") {
+  using namespace modelconv;
+  using namespace gfxminimath;
   Assimp::Importer importer;
-  const auto scene = importer.ReadFile("filename", 0);
-  CHECK_EQ(scene, nullptr);
+  const auto scene = importer.ReadFile("donut2022.fbx",
+                                       aiProcess_MakeLeftHanded 
+                                       | aiProcess_FlipWindingOrder 
+                                       | aiProcess_Triangulate 
+                                       | aiProcess_CalcTangentSpace
+                                       | aiProcess_JoinIdenticalVertices
+                                       | aiProcess_ValidateDataStructure
+                                       | aiProcess_FixInfacingNormals
+                                       | aiProcess_SortByPType
+                                       | aiProcess_GenSmoothNormals
+                                       | aiProcess_FindInvalidData
+                                       | aiProcess_GenUVCoords
+                                       | aiProcess_TransformUVCoords
+                                       | aiProcess_FindInstances
+                                       | aiProcess_Debone
+                                       | aiProcess_EmbedTextures);
+  // consider using meshoptimizer(https://github.com/zeux/meshoptimizer) for mesh optimizations.
+  CHECK_UNARY(scene != nullptr);
+  CHECK_UNARY((scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE) == 0);
+  CHECK_UNARY(scene->HasMeshes());
+  std::vector<matrix> transform_matrix;
 }
