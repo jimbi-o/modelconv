804cc0f1c14e614eacb732e0450fcc854929e7f3
jimbi-o <35837796+jimbi-o@users.noreply.github.com>
2022-11-17 09:56:26 +0900

    flatten model transform matrices

--- src/modelconv.cpp
+++ src/modelconv.cpp
@@ -1,8 +1,56 @@
 #include "modelconv/modelconv.h"
+#include <vector>
+#include <utility>
 #include "assimp/Importer.hpp"
-#include "assimp/scene.h"
 #include "assimp/postprocess.h"
+#include "assimp/scene.h"
 #include "doctest/doctest.h"
+namespace modelconv {
+namespace {
+const uint32_t kInvalidIndex = ~0U;
+using namespace Assimp;
+struct PerDrawCallModelIndexSet {
+  std::vector<uint32_t> transform_matrix_index_list;
+};
+struct FlattenedModelData {
+  std::vector<aiMatrix4x4> transform_matrix_list;
+};
+auto GetUint32(const std::size_t s) {
+  return static_cast<uint32_t>(s);
+}
+void PushTransformMatrix(const aiNode* node,
+                         const uint32_t parent_transform_index,
+                         const aiMatrix4x4& parent_transform,
+                         PerDrawCallModelIndexSet* per_draw_call_model_index_set,
+                         std::vector<aiMatrix4x4>* transform_matrix_list) {
+  auto transform_index = parent_transform_index;
+  auto transform = parent_transform;
+  if (!node->mTransformation.IsIdentity()) {
+    transform_index = kInvalidIndex;
+    transform *= node->mTransformation;
+  }
+  if (node->mNumMeshes > 0) {
+    if (transform_index == kInvalidIndex) {
+      transform_index = GetUint32((*transform_matrix_list).size());
+      (*transform_matrix_list).push_back(transform);
+    }
+    for (uint32_t i = 0; i < node->mNumMeshes; i++) {
+      per_draw_call_model_index_set[node->mMeshes[i]].transform_matrix_index_list.push_back(transform_index);
+    }
+  }
+  for (uint32_t i = 0; i < node->mNumChildren; i++) {
+    PushTransformMatrix(node->mChildren[i], transform_index, transform, per_draw_call_model_index_set, transform_matrix_list);
+  }
+}
+auto FlattenModelDataPerDrawCall(const aiScene* scene) {
+  std::vector<PerDrawCallModelIndexSet> per_draw_call_model_index_set(scene->mNumMeshes);
+  FlattenedModelData flattened_model_data;
+  aiMatrix4x4 transform_matrix;
+  PushTransformMatrix(scene->mRootNode, kInvalidIndex, transform_matrix, per_draw_call_model_index_set.data(), &flattened_model_data.transform_matrix_list);
+  return std::make_pair(per_draw_call_model_index_set, flattened_model_data);
+}
+} // namespace anonymous
+} // namespace modelconv
 TEST_CASE("load model") {
   using namespace modelconv;
   Assimp::Importer importer;
@@ -27,4 +75,5 @@ TEST_CASE("load model") {
   CHECK_EQ((scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE), 0);
   CHECK_UNARY(scene->HasMeshes());
   CHECK_NE(scene->mRootNode, nullptr);
+  FlattenModelDataPerDrawCall(scene);
 }
