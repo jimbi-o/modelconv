77df47174eb7609d3288688255e409d6a101ec2a
jimbi-o <35837796+jimbi-o@users.noreply.github.com>
2022-11-19 13:48:28 +0900

    refactor structs

--- src/modelconv.cpp
+++ src/modelconv.cpp
@@ -27,16 +27,10 @@ namespace modelconv {
 namespace {
 using namespace Assimp;
 const uint32_t kInvalidIndex = ~0U;
-// data required by CPU per draw call
 struct PerDrawCallModelIndexSet {
   std::vector<uint32_t> transform_matrix_index_list;
   uint32_t index_buffer_offset;
   uint32_t index_buffer_len;
-};
-// binaries bound to GPU on draw
-struct FlattenedModelData {
-  std::vector<aiMatrix4x4> transform_matrix_list;
-  std::vector<uint32_t> index_buffer;
 };
 auto GetUint32(const std::size_t s) {
   return static_cast<uint32_t>(s);
@@ -65,9 +59,9 @@ void PushTransformMatrix(const aiNode* node,
     PushTransformMatrix(node->mChildren[i], transform_index, transform, per_draw_call_model_index_set, transform_matrix_list);
   }
 }
-void GatherMeshData(const uint32_t mesh_num, const aiMesh* const * meshes,
-                    std::vector<PerDrawCallModelIndexSet>* per_draw_call_model_index_set,
-                    std::vector<uint32_t>* index_buffer) {
+auto GatherMeshData(const uint32_t mesh_num, const aiMesh* const * meshes,
+                    std::vector<PerDrawCallModelIndexSet>* per_draw_call_model_index_set) {
+  std::vector<uint32_t> index_buffer;
   for (uint32_t i = 0; i < mesh_num; i++) {
     auto mesh = meshes[i];
     if (!mesh->HasFaces()) { continue; }
@@ -78,10 +72,10 @@ void GatherMeshData(const uint32_t mesh_num, const aiMesh* const * meshes,
     // per mesh index data
     const uint32_t kTriangleVertexNum = 3;
     auto& per_mesh_data = (*per_draw_call_model_index_set)[i];
-    per_mesh_data.index_buffer_offset = static_cast<uint32_t>(index_buffer->size());
+    per_mesh_data.index_buffer_offset = static_cast<uint32_t>(index_buffer.size());
     per_mesh_data.index_buffer_len    = mesh->mNumFaces * kTriangleVertexNum;
     // push mesh index buffer
-    index_buffer->reserve(per_mesh_data.index_buffer_offset + per_mesh_data.index_buffer_len);
+    index_buffer.reserve(per_mesh_data.index_buffer_offset + per_mesh_data.index_buffer_len);
     for (uint32_t j = 0; j < mesh->mNumFaces; j++) {
       const auto& face = mesh->mFaces[j];
       if (face.mNumIndices != kTriangleVertexNum) {
@@ -89,21 +83,14 @@ void GatherMeshData(const uint32_t mesh_num, const aiMesh* const * meshes,
         continue;
       }
       for (uint32_t k = 0; k < kTriangleVertexNum; k++) {
-        index_buffer->push_back(face.mIndices[k]);
+        index_buffer.push_back(face.mIndices[k]);
       }
     }
-    assert(index_buffer->size() == per_mesh_data.index_buffer_offset + per_mesh_data.index_buffer_len);
+    assert(index_buffer.size() == per_mesh_data.index_buffer_offset + per_mesh_data.index_buffer_len);
   }
+  return index_buffer;
 }
-auto FlattenModelDataPerDrawCall(const aiScene* scene) {
-  std::vector<PerDrawCallModelIndexSet> per_draw_call_model_index_set(scene->mNumMeshes);
-  FlattenedModelData flattened_model_data;
-  aiMatrix4x4 transform_matrix;
-  PushTransformMatrix(scene->mRootNode, kInvalidIndex, transform_matrix, per_draw_call_model_index_set.data(), &flattened_model_data.transform_matrix_list);
-  GatherMeshData(scene->mNumMeshes, scene->mMeshes, &per_draw_call_model_index_set, &flattened_model_data.index_buffer);
-  return std::make_pair(per_draw_call_model_index_set, flattened_model_data);
-}
-auto GetFlattenedTransformList(const std::vector<aiMatrix4x4>& matrix_list) {
+auto GetFlattenedMatrixList(const std::vector<aiMatrix4x4>& matrix_list) {
   if (matrix_list.empty()) { return std::vector<float>{}; }
   assert(sizeof(matrix_list[0].a1) == 4);
   const auto kMatrixNum = GetUint32(matrix_list.size());
@@ -121,10 +108,6 @@ auto OutputBinaryToFile(const size_t file_size_in_byte, const void* buffer, cons
   snprintf(filename, kFilenameLen, "%s%s", filename_base, filename_option);
   std::ofstream output_file(filename, std::ios::out | std::ios::binary);
   output_file.write(reinterpret_cast<const char*>(buffer), file_size_in_byte);
-}
-auto WriteBinaryData(const char* const filename, const FlattenedModelData& flattened_model_data) {
-  const auto transform_matrix_list_binary = GetFlattenedTransformList(flattened_model_data.transform_matrix_list);
-  OutputBinaryToFile(transform_matrix_list_binary.size() * sizeof(float), transform_matrix_list_binary.data(), filename, ".transform_matrix.bin");
 }
 } // namespace anonymous
 } // namespace modelconv
@@ -153,6 +136,16 @@ TEST_CASE("load model") {
   CHECK_EQ((scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE), 0);
   CHECK_UNARY(scene->HasMeshes());
   CHECK_NE(scene->mRootNode, nullptr);
-  auto [per_draw_call_model_index_set, flattened_model_data] = FlattenModelDataPerDrawCall(scene);
-  WriteBinaryData(filename, flattened_model_data);
+  std::vector<PerDrawCallModelIndexSet> per_draw_call_model_index_set(scene->mNumMeshes);
+  {
+    // output transform matrix binaries
+    std::vector<aiMatrix4x4> transform_matrix_list;
+    aiMatrix4x4 transform_matrix;
+    PushTransformMatrix(scene->mRootNode, kInvalidIndex, transform_matrix, per_draw_call_model_index_set.data(), &transform_matrix_list);
+    const auto transform_matrix_list_binary = GetFlattenedMatrixList(transform_matrix_list);
+    OutputBinaryToFile(transform_matrix_list_binary.size() * sizeof(float), transform_matrix_list_binary.data(), filename, ".transform_matrix.bin");
+  }
+  {
+    auto index_buffer = GatherMeshData(scene->mNumMeshes, scene->mMeshes, &per_draw_call_model_index_set);
+  }
 }
