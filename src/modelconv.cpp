b6f85b04d2ce0ee4c547387afe3af3f78a38ef02
jimbi-o <35837796+jimbi-o@users.noreply.github.com>
2022-11-19 17:42:59 +0900

    vertex buffer output added

--- src/modelconv.cpp
+++ src/modelconv.cpp
@@ -29,8 +29,9 @@ using namespace Assimp;
 const uint32_t kInvalidIndex = ~0U;
 struct PerDrawCallModelIndexSet {
   std::vector<uint32_t> transform_matrix_index_list;
-  uint32_t index_buffer_offset;
-  uint32_t index_buffer_len;
+  uint32_t index_buffer_offset{0};
+  uint32_t index_buffer_len{0};
+  uint32_t vertex_buffer_offset{0};
 };
 auto GetUint32(const std::size_t s) {
   return static_cast<uint32_t>(s);
@@ -59,9 +60,24 @@ void PushTransformMatrix(const aiNode* node,
     PushTransformMatrix(node->mChildren[i], transform_index, transform, per_draw_call_model_index_set, transform_matrix_list);
   }
 }
+struct MeshBuffers {
+  std::vector<uint32_t> index_buffer;
+  std::vector<float> vertex_buffer_position;
+  std::vector<float> vertex_buffer_normal;
+  std::vector<float> vertex_buffer_tangent;
+  std::vector<float> vertex_buffer_bitangent;
+  std::vector<float> vertex_buffer_texcoord;
+  std::vector<float> vertex_buffer_color;
+};
 auto GatherMeshData(const uint32_t mesh_num, const aiMesh* const * meshes,
                     std::vector<PerDrawCallModelIndexSet>* per_draw_call_model_index_set) {
   std::vector<uint32_t> index_buffer;
+  std::vector<float> vertex_buffer_position;
+  std::vector<float> vertex_buffer_normal;
+  std::vector<float> vertex_buffer_tangent;
+  std::vector<float> vertex_buffer_bitangent;
+  std::vector<float> vertex_buffer_texcoord;
+  std::vector<float> vertex_buffer_color;
   for (uint32_t i = 0; i < mesh_num; i++) {
     auto mesh = meshes[i];
     if (!mesh->HasFaces()) { continue; }
@@ -69,13 +85,13 @@ auto GatherMeshData(const uint32_t mesh_num, const aiMesh* const * meshes,
       logwarn("invalid primitive type {}", mesh->mPrimitiveTypes);
       continue;
     }
-    // per mesh index data
-    const uint32_t kTriangleVertexNum = 3;
     auto& per_mesh_data = (*per_draw_call_model_index_set)[i];
-    per_mesh_data.index_buffer_offset = static_cast<uint32_t>(index_buffer.size());
-    per_mesh_data.index_buffer_len    = mesh->mNumFaces * kTriangleVertexNum;
     {
-      // push mesh index buffer
+      // per mesh index data
+      const uint32_t kTriangleVertexNum = 3;
+      per_mesh_data.index_buffer_offset = static_cast<uint32_t>(index_buffer.size());
+      per_mesh_data.index_buffer_len    = mesh->mNumFaces * kTriangleVertexNum;
+      // index buffer
       index_buffer.reserve(per_mesh_data.index_buffer_offset + per_mesh_data.index_buffer_len);
       for (uint32_t j = 0; j < mesh->mNumFaces; j++) {
         const auto& face = mesh->mFaces[j];
@@ -89,8 +105,28 @@ auto GatherMeshData(const uint32_t mesh_num, const aiMesh* const * meshes,
       }
       assert(index_buffer.size() == per_mesh_data.index_buffer_offset + per_mesh_data.index_buffer_len);
     }
+    {
+      // per mesh vertex buffer data
+      per_mesh_data.vertex_buffer_offset = static_cast<uint32_t>(vertex_buffer_position.size());
+      // position buffer
+      const uint32_t kPositionBufferComponentNum = 3;
+      vertex_buffer_position.reserve(per_mesh_data.vertex_buffer_offset + mesh->mNumVertices * kPositionBufferComponentNum);
+      for (uint32_t j = 0; j < mesh->mNumVertices; j++) {
+        const auto& vertex = mesh->mVertices[j];
+        vertex_buffer_position.insert(vertex_buffer_position.end(), {vertex.x, vertex.y, vertex.z});
+      }
+      assert(vertex_buffer_position.size() == per_mesh_data.vertex_buffer_offset + mesh->mNumVertices * kPositionBufferComponentNum);
+    }
   }
-  return index_buffer;
+  return MeshBuffers{
+    index_buffer,
+    vertex_buffer_position,
+    vertex_buffer_normal,
+    vertex_buffer_tangent,
+    vertex_buffer_bitangent,
+    vertex_buffer_texcoord,
+    vertex_buffer_color,
+  };
 }
 auto GetFlattenedMatrixList(const std::vector<aiMatrix4x4>& matrix_list) {
   if (matrix_list.empty()) { return std::vector<float>{}; }
@@ -137,7 +173,8 @@ TEST_CASE("load model") {
                                        | aiProcess_TransformUVCoords
                                        | aiProcess_FindInstances
                                        | aiProcess_Debone
-                                       | aiProcess_EmbedTextures);
+                                       | aiProcess_EmbedTextures
+                                       | aiProcess_RemoveRedundantMaterials);
   // consider using meshoptimizer (https://github.com/zeux/meshoptimizer) for mesh optimizations.
   CHECK_NE(scene, nullptr);
   CHECK_EQ((scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE), 0);
@@ -153,7 +190,14 @@ TEST_CASE("load model") {
     OutputBinaryToFile(transform_matrix_list_binary, filename, ".transform_matrix.bin");
   }
   {
-    auto index_buffer = GatherMeshData(scene->mNumMeshes, scene->mMeshes, &per_draw_call_model_index_set);
+    auto [index_buffer, vertex_buffer_position, vertex_buffer_normal, vertex_buffer_tangent, vertex_buffer_bitangent, vertex_buffer_texcoord, vertex_buffer_color] = GatherMeshData(scene->mNumMeshes, scene->mMeshes, &per_draw_call_model_index_set);
     OutputBinaryToFile(index_buffer, filename, ".index_buffer.bin");
+    OutputBinaryToFile(vertex_buffer_position, filename, ".vertex_buffer_position.bin");
+    OutputBinaryToFile(vertex_buffer_normal, filename, ".vertex_buffer_normal.bin");
+    OutputBinaryToFile(vertex_buffer_tangent, filename, ".vertex_buffer_tangent.bin");
+    OutputBinaryToFile(vertex_buffer_bitangent, filename, ".vertex_buffer_bitangent.bin");
+    OutputBinaryToFile(vertex_buffer_texcoord, filename, ".vertex_buffer_texcoord.bin");
+    OutputBinaryToFile(vertex_buffer_color, filename, ".vertex_buffer_color.bin");
+    // TODO gather buffer size and filepath info
   }
 }
