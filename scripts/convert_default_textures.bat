3b5730dd59d7c614f741df0719c6c2a84a8d2240
jimbi-o <35837796+jimbi-o@users.noreply.github.com>
2022-12-17 23:42:52 +0900

    output emissive texture as srgb

--- scripts/convert_default_textures.bat
+++ scripts/convert_default_textures.bat
@@ -1,5 +1,6 @@
 :: run from project dir
+:: cmd.exe /c scripts\\convert_default_textures.bat "D:\dev\tools\texconv" "resources\output\textures"
 set TEXCONV=%1
 set OUTDIR=%2
-%TEXCONV% -f BC7_UNORM_SRGB -srgb -o %OUTDIR% -y -sepalpha -keepcoverage 0.75 -dx10 -nologo resources\textures\black.png
-%TEXCONV% -f BC7_UNORM -srgb -o %OUTDIR% -y -sepalpha -keepcoverage 0.75 -dx10 -nologo resources\textures\white.png resources\textures\normal.png resources\textures\yellow.png
+%TEXCONV% -f BC7_UNORM_SRGB -srgb -o %OUTDIR% -y -sepalpha -keepcoverage 0.75 -dx10 -nologo resources\textures\black.png resources\textures\white.png
+%TEXCONV% -f BC7_UNORM -srgb -o %OUTDIR% -y -sepalpha -keepcoverage 0.75 -dx10 -nologo resources\textures\normal.png resources\textures\yellow.png
